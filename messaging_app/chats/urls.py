bdccd56ebc7b7ab2247fff813b0a198a9e705f97
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-09-21 14:21:44 +0300

    Update serializer.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
+from rest_framework import routers
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api/', include('chats.urls')),
 ]
