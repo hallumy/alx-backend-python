349bf8f4631fa67e6867ffc521b65500f0be4b34
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-09-28 10:52:07 +0300

    Update setting

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -58,6 +58,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
          'rest_framework_simplejwt.authentication.JWTAuthentication',
          'rest_framework.authentication.BasicAuthentication',
+         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
